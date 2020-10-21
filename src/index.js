77b6af5a479820e1cb43a0f624883262fa9c5c17
swaggerloaf <swaggerloaf@gmail.com>
2020-10-21 17:50:37 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
-import "bootstrap/dist/css/bootstrap.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 const Beverage = () => (
   <div>
     <FontAwesomeIcon icon={faCheckSquare} />
@@ -10,5 +11,5 @@ const Beverage = () => (
   </div>
 );
 
-const domElement = document.getElementById("root");
+const domElement = document.getElementById('root');
 ReactDOM.render(<Beverage></Beverage>, domElement);
