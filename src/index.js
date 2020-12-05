16dc66edfa6965dba3b63344deb4fdd81b59f7ee
swaggerloaf <swaggerloaf@gmail.com>
2020-12-05 13:34:51 -0500

    added bootstrap sass

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import './style.scss';
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,8 +6,10 @@ import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 const Beverage = () => (
   <div>
-    <FontAwesomeIcon icon={faCheckSquare} />
-    Your <FontAwesomeIcon icon={faCoffee} /> is hot and ready!
+    <p class="text-danger">.text-danger</p>
+    <span className="text-info">
+      Your <FontAwesomeIcon icon={faCoffee} /> is hot and ready!
+    </span>
   </div>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,61 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 module.exports = {
-  entry: "./src/index",
-  mode: "development",
+  entry: './src/index',
+  mode: 'development',
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     port: 3001,
   },
   output: {
-    publicPath: "http://localhost:3001/",
+    publicPath: 'http://localhost:3001/',
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.(scss)$/,
+        use: [
+          {
+            // inject CSS to page
+            loader: 'style-loader',
+          },
+          {
+            // translates CSS into CommonJS modules
+            loader: 'css-loader',
+          },
+          {
+            // Run postcss actions
+            loader: 'postcss-loader',
+            options: {
+              // `postcssOptions` is needed for postcss 8.x;
+              // if you use postcss 7.x skip the key
+              postcssOptions: {
+                // postcss plugins, can be exported to postcss.config.js
+                plugins: function () {
+                  return [require('autoprefixer')];
+                },
+              },
+            },
+          },
+          {
+            // compiles Sass to CSS
+            loader: 'sass-loader',
+          },
+        ],
       },
       {
         test: /\.jsx?$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
-          presets: ["@babel/preset-react"],
+          presets: ['@babel/preset-react'],
         },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
   ],
 };
